715dab0df885936bd3a20364a0cbe6f0ccdd5e36
Ehfaz Rezwan <ehfaz.rezwan@optimizely.com>
2026-04-06 17:16:16 +0600

    feat(llm): add GatewayClient for LLM gateway integration

--- tradingagents/llm_clients/__init__.py
+++ tradingagents/llm_clients/__init__.py
@@ -1,4 +1,5 @@
 from .base_client import BaseLLMClient
 from .factory import create_llm_client
+from .gateway_client import GatewayClient
 
-__all__ = ["BaseLLMClient", "create_llm_client"]
+__all__ = ["BaseLLMClient", "GatewayClient", "create_llm_client"]
